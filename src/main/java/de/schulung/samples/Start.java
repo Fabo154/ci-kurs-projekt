149f0f61cd5bd1f0e3ef7fc32d45373bf20ac3c5
Fabio <fabio@mail.de>
2022-09-14 15:07:16 +0200

    Lösche Ausgabe

--- src/main/java/de/schulung/samples/Start.java
+++ src/main/java/de/schulung/samples/Start.java
@@ -5,6 +5,5 @@ public class Start {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Goodbye World");
-		System.out.println("Close now");
 	}
 }
